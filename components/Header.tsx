53a257ec7f43efa220e3e5fa14d2fd9475405183
Arman Buludyan <buludian.arman@gmail.com>
2022-12-05 19:56:19 +0400

    header component

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import { MagnifyingGlassIcon, BellIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+          alt="logo"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex item-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            alt="account"
+            src="https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
