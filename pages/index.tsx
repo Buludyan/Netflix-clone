eadde7b0e3f7eed261f82d0a9c06385c445817bf
Arman Buludyan <buludian.arman@gmail.com>
2022-12-07 17:28:06 +0400

    create Row component, some css fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Row from "../components/Row";
 import { Movie } from "../typings";
 import requests from "../utils/requests";
 
@@ -33,10 +34,19 @@ const Home = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         <Banner netflixOriginals={netflixOriginals} />
       </main>
-      <section></section>
+      <section className="md:space-y-24 pl-4 lg:pl-16">
+        <Row title="Trending Now" movies={trendingNow} />
+        <Row title="Top Rated" movies={topRated} />
+        <Row title="Action Thrillers" movies={actionMovies} />
+        {/* My List */}
+        <Row title="Comedies" movies={comedyMovies} />
+        <Row title="Scary Movies" movies={horrorMovies} />
+        <Row title="Romance Movies" movies={romanceMovies} />
+        <Row title="Documentaries" movies={documentaries} />
+      </section>
     </div>
   );
 };
